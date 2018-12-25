6f20716bc145740d03a18333d9f91a4de4b96c38
aliszhuravl <alis.zhuravl@gmail.com>
2018-12-26 02:43:30 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+//= parts/_viewport.js
+//= parts/_ie.js
+//= parts/_link-highlighting.js
+//= parts/_scrolling.js
+//= parts/_slider.js
+//= parts/_burger.js
+//= parts/_accordion.js
+//= parts/_dropdown.js
+//= parts/_map.js
+//= parts/_tabs.js
+//= parts/_fancybox.js
+//= parts/_search.js
+//= parts/_scroll.js
