876225a336693112d065916534cf761be6d6b77f
aliszhuravl <alis.zhuravl@gmail.com>
2019-01-30 16:46:26 +0300

    reload

--- src/js/parts/_scroll.js
+++
@@ -1,9 +0,0 @@
-
-$( window ).scroll(function() {
-    if ( $(window).scrollTop() > 200) {
-        $(".btn_menu").addClass("btn_show");
-    }
-    if ( $(window).scrollTop() < 200) {
-        $(".btn_menu").removeClass("btn_show");
-    }
-});
